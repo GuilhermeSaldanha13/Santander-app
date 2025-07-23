58ab30ce9243c461b16653e4a865a05c46767854
GuilhermeSaldanha13 <guisaldanhatk@gmail.com>
2025-07-23 10:45:14 +0200

    Primeiro commit - Santander-app

---
+++ src/main/java/me/dio/santander_app/SantanderAppApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santander_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santander_app/SantanderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
