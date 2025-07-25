8e9cf879d51b8c7532d888dd6a0af6d60eed94ae
GuilhermeSaldanha13 <guisaldanhatk@gmail.com>
2025-07-25 12:04:50 +0200

    Implementação da camada de dominio

---
+++ src/main/java/me/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package me.domain.repository;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
